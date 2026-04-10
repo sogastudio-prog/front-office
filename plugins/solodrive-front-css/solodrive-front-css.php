624b901705cd80da6a4330e01d69796afc5dc805
SogaStudio <sogacapital@gmail.com>
2026-04-10 18:56:57 -0400

    x

---
+++ plugins/solodrive-front-css/solodrive-front-css.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin Name: SoloDrive Front CSS
+ * Description: Structured frontend stylesheet loader for SoloDrive marketing and onboarding flow.
+ * Version: 0.1.0
+ * Author: SoloDrive
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+define( 'SD_FRONT_CSS_VERSION', '0.1.0' );
+define( 'SD_FRONT_CSS_URL', plugin_dir_url( __FILE__ ) );
+
+function sd_front_css_enqueue_assets() {
+	if ( is_admin() ) {
+		return;
+	}
+
+	$files = array(
+		'01-tokens.css',
+		'02-base.css',
+		'03-astra-overrides.css',
+		'04-site-pages.css',
+		'05-front-flow.css',
+		'06-forms.css',
+		'07-footer.css',
+		'08-responsive.css',
+	);
+
+	$deps = array();
+
+	foreach ( $files as $file ) {
+		$handle = 'sd-front-css-' . sanitize_title( str_replace( '.css', '', $file ) );
+
+		wp_enqueue_style(
+			$handle,
+			SD_FRONT_CSS_URL . 'assets/css/' . $file,
+			$deps,
+			SD_FRONT_CSS_VERSION
+		);
+
+		$deps = array( $handle );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'sd_front_css_enqueue_assets', 20 );
